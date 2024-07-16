70f87de8bd024eaff110c072ee3f23c4342798d1
daisy <daisybb545@gmail.com>
2024-07-16 14:30:53 +0630

    sample-script finish

---
+++ Basic.js
@@ -0,0 +1,79 @@
+// //1:Variable
+// //Var
+// var name = "Ko Ko";
+// var age = 29;
+// var isStudent = true;
+
+// //Let
+// let city = "Yangon";
+// let area = 45.8;
+
+// //const
+// const country = "Burma";
+// const Bq = 9;
+
+// //Display the values in the console
+// console.log(name);
+// console.log(age);
+// console.log(isStudent);
+// console.log(city);
+// console.log(area);
+// console.log(country);
+// console.log(Bq);
+
+
+// //2.Type
+// var temp = 72;//Number
+// let greet = "Hello, How are you?";//String
+// const isRaing = false;//Boolean
+// var empty = null;//Null
+// let notAssign; //Undified
+// let noAssign;//Undified
+
+// let person = {name: "Mg Mg", age: 29};//Object
+// let fruits = ['Apple', 'Banana', 'Mango'];//Array
+
+// //Check in console using 'typeof' keyword
+// console.log(temp);
+// console.log(greet);
+// console.log(isRaing);
+// console.log(empty);
+// console.log(notAssign);
+// console.log(noAssign);
+// console.log(person);
+// console.log(fruits);
+
+
+// //3.Arithmetic Operators
+// var sum = 5 + 2;//7
+// let diff = 10 - 5;//5
+// var product = 3 * 7//21
+// let divide =10 / 2;//5  ရလဒ်
+// const remainder =20 % 5;//0 အကြွင်း
+
+
+// //4. Assignment Operarors
+// var x = 10;
+// x = x + 5;// x += 5; အတူတူပဲ
+// x *= 2;// x = x * 2; အတူတူပဲ
+
+
+// //5.Comparison Operators
+// 5 == '5'//true  ဂဏန်း 5 နဲ့ စာငါး အတူတူပါ
+// 5 === '5'//false  
+// 5 != '5';//false  တန်ဖိုး
+// 5 !== '5';//true  အမျိုးအစား
+
+
+//6.Logical Operators
+(true&&false)// false  &&mean=and-နှင့်
+(true||false)// true  နှစ်ခုထဲကတစ်ခု=or-သို့မဟုတ်
+(!true)//false
+
+//7.String Operators
+var we = "Hello";
+let name = "Ko Ko";
+let welcome = greet +', ' + name + '!';
+
+//8.Type Operator
+//typeof
